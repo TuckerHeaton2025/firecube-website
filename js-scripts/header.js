6db6272138417fd24d0a96fee05502e5c956626b
Tucker Heaton <tuckerheaton34@gmail.com>
2025-09-19 09:37:16 -0600

    Changed navbar jqeuery for hover
    On screens over 1600 px

--- js-scripts/header.js
+++ js-scripts/header.js
@@ -1,37 +1,42 @@
 // Check if the device is touchscreen (NOT SURE)
 function isTouchDevice() {
-  return (('ontouchstart' in window) ||
-     (navigator.maxTouchPoints > 0) ||
-     (navigator.msMaxTouchPoints > 0));
+    return (('ontouchstart' in window) ||
+        (navigator.maxTouchPoints > 0) ||
+        (navigator.msMaxTouchPoints > 0));
 }
 
 
 //jquery for toggle dropdown menus
-if(isTouchDevice()){
+if (isTouchDevice()) {
+    var windowsize = $(window).width();
 
-    $(document).ready(function () {
-        //toggle sub-menus
-        $(".sub-btn").click(function () {
-            // If the clicked sub-menu is already open, close it
-            if ($(this).next(".sub-menu").is(":visible")) {
-                $(this).next(".sub-menu").slideUp();
-            } else {
-                // Close all other sub-menus first
-                $(".sub-menu, .more-menu").slideUp();
-                // Then open the clicked one
-                $(this).next(".sub-menu").slideDown();
-            }
-        });
 
-        //toggle more-menus
-        $(".more-btn").click(function () {
-            $(this).next(".more-menu").slideToggle();
+    if (windowsize <= 1060) {
+
+        $(document).ready(function () {
+            //toggle sub-menus
+            $(".sub-btn").click(function () {
+                // If the clicked sub-menu is already open, close it
+                if ($(this).next(".sub-menu").is(":visible")) {
+                    $(this).next(".sub-menu").slideUp();
+                } else {
+                    // Close all other sub-menus first
+                    $(".sub-menu, .more-menu").slideUp();
+                    // Then open the clicked one
+                    $(this).next(".sub-menu").slideDown();
+                }
+            });
+
+            //toggle more-menus
+            $(".more-btn").click(function () {
+                $(this).next(".more-menu").slideToggle();
+            });
+
+            $(".close-btn").click(function () {
+                $(".sub-menu, .more-menu").slideUp();  // Hide all dropdown menus
+            });
         });
-        
-        $(".close-btn").click(function () {
-            $(".sub-menu, .more-menu").slideUp();  // Hide all dropdown menus
-        });
-    });
+    }
 }
 //javascript for the responsive navigation menu
 var menu = document.querySelector(".menu");
